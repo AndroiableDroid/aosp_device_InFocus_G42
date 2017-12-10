60a9d7de730476bf27b16cce41cd9186391f932d
Mohd Faraz <mohd.faraz.abc@gmail.com>
2017-12-10 21:06:23 +0530

    G42: INITIAL

---
+++ full_G42.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, device/InFocus/G42/device_G42.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := G42
+PRODUCT_NAME := full_G42
+PRODUCT_BRAND := InFocus
+PRODUCT_MODEL := Thor
+PRODUCT_MANUFACTURER := InFocus
